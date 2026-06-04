e4a8511de3bd8e3c2dbcf0d26e3de3f1905ba004
q2df <nalut41@gmail.com>
2026-06-04 12:04:31 +0000

    make login screen simpler,fix bug in report.html where report as pdf was not working,add app icons

--- main.js
+++ main.js
@@ -7,6 +7,7 @@ const createWindow = () => {
   const win = new BrowserWindow({
     width: 1200, // عرض أكبر ليناسب لوحة التحكم
     height: 800,
+    icon: path.join(__dirname, './assets/icon.png'),
     webPreferences: {
       // ربط ملف الجسر الآمن
       preload: path.join(__dirname, 'preload.js'),
